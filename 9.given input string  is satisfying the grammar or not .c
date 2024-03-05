77dfe4f7d44325e26230941508744bb570455d37
Vishnuvardhanreddy bandi <124245030+Vishnureddy0404@users.noreply.github.com>
2024-03-05 23:51:33 +0530

    Update 9.given input string  is satisfying the grammar or not .c

--- 9.given input string  is satisfying the grammar or not .c
+++ 9.given input string  is satisfying the grammar or not .c
@@ -1,47 +1,83 @@
-#include <stdio.h>
-#include <string.h>
-
-// Function to check if the input string satisfies the grammar
-int checkGrammar(char *input) {
-    int length = strlen(input);
-    int count_a = 0, count_b = 0;
-
-    // Count the number of 'a's and 'b's in the input string
-    for(int i = 0; i < length; i++) {
-        if(input[i] == 'a') {
-            count_a++;
-        }
-        else if(input[i] == 'b') {
-            count_b++;
-        }
-        else {
-            // If the input contains any other characters apart from 'a' and 'b', return 0
-            return 0;
-        }
+#include<stdio.h>
+#include<ctype.h>
+void FIRST(char[],char );
+void addToResultSet(char[],char);
+int numOfProductions;
+char productionSet[10][10];
+int main()
+{
+    int i;
+    char choice;
+    char c;
+    char result[20];
+    printf("How many number of productions ? :");
+    scanf(" %d",&numOfProductions);
+    for(i=0;i<numOfProductions;i++)//read production string eg: E=E+T
+    {
+        printf("Enter productions Number %d : ",i+1);
+        scanf(" %s",productionSet[i]);
     }
-    
-    // If the count of 'a's and 'b's are equal and both are greater than 0, return 1 (true)
-    if(count_a == count_b && count_a > 0) {
-        return 1;
+    do
+    {
+        printf("\n Find the FIRST of  :");
+        scanf(" %c",&c);
+        FIRST(result,c); //Compute FIRST; Get Answer in 'result' array
+        printf("\n FIRST(%c)= { ",c);
+        for(i=0;result[i]!='\0';i++)
+        printf(" %c ",result[i]);       //Display result
+        printf("}\n");
+         printf("press 'y' to continue : ");
+        scanf(" %c",&choice);
     }
-    
-    // Otherwise, return 0 (false)
-    return 0;
+    while(choice=='y'||choice =='Y');
 }
-
-int main() {
-    char input[100];
-    
-    // Get input from the user
-    printf("Enter a string: ");
-    scanf("%s", input);
-    
-    // Check if the input string satisfies the grammar
-    if(checkGrammar(input)) {
-        printf("The input string satisfies the grammar.\n");
-    } else {
-        printf("The input string does not satisfy the grammar.\n");
+void FIRST(char* Result,char c)
+{
+    int i,j,k;
+    char subResult[20];
+    int foundEpsilon;
+    subResult[0]='\0';
+    Result[0]='\0';
+    if(!(isupper(c)))
+    {
+        addToResultSet(Result,c);
+               return ;
     }
-    
-    return 0;
+    for(i=0;i<numOfProductions;i++)
+    {
+        if(productionSet[i][0]==c)
+        {
+ if(productionSet[i][2]=='$') addToResultSet(Result,'$');           
+      else
+            {
+                j=2;
+                while(productionSet[i][j]!='\0')
+                {
+                foundEpsilon=0;
+                FIRST(subResult,productionSet[i][j]);
+                for(k=0;subResult[k]!='\0';k++)
+                    addToResultSet(Result,subResult[k]);
+                 for(k=0;subResult[k]!='\0';k++)
+                     if(subResult[k]=='$')
+                     {
+                         foundEpsilon=1;
+                         break;
+                     }
+                 if(!foundEpsilon)
+                     break;
+                 j++;
+                }
+            }
+    }
+}
+    return ;
+}
+void addToResultSet(char Result[],char val)
+{
+    int k;
+    for(k=0 ;Result[k]!='\0';k++)
+        if(Result[k]==val)
+            return;
+    Result[k]=val;
+    Result[k+1]='\0';
 }
