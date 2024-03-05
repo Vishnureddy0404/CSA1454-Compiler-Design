ae4878a9e59ebbf1a4621ce2e43a17d107ccc578
Vishnuvardhanreddy bandi <124245030+Vishnureddy0404@users.noreply.github.com>
2024-03-05 05:53:04 +0530

    6.C program to eliminate left recursion.c

--- 6.C program to eliminate left recursion.c
+++ 6.C program to eliminate left recursion.c
@@ -1,37 +1,41 @@
 #include<stdio.h>  
 #include<string.h>  
-#define SIZE 10  
-  int main () {  
-       char non_terminal;  
-       char beta,alpha;  
-       int num;  
-       char production[10][SIZE];  
-       int index=3; /* starting of the string following "->" */  
-       printf("Enter Number of Production : ");  
-       scanf("%d",&num);  
-       printf("Enter the grammar as E->E-A :\n");  
-       for(int i=0;i<num;i++){  
-            scanf("%s",production[i]);  
-       }  
-       for(int i=0;i<num;i++){  
-            printf("\nGRAMMAR : : : %s",production[i]);  
-            non_terminal=production[i][0];  
-            if(non_terminal==production[i][index]) {  
-                 alpha=production[i][index+1];  
-                 printf(" is left recursive.\n");  
-                 while(production[i][index]!=0 && production[i][index]!='|')  
-                      index++;  
-                 if(production[i][index]!=0) {  
-                      beta=production[i][index+1];  
-                      printf("Grammar without left recursion:\n");  
-                      printf("%c->%c%c\'",non_terminal,beta,non_terminal);  
-                      printf("\n%c\'->%c%c\'|E\n",non_terminal,alpha,non_terminal);  
-                 }  
-                 else  
-                      printf(" can't be reduced\n");  
+//#define SIZE 10  
+int main ()
+{  
+    char non,be,al;  
+    int num;  
+    char pro[10][10];  
+    int index=3,i;  
+    printf("Enter Number of Production : ");  
+    scanf("%d",&num);  
+    printf("Enter the grammar as E->E-A :\n");  
+    for(i=0;i<num;i++)
+	{  
+        scanf("%s",pro[i]);  
+    }  
+    for(i=0;i<num;i++)
+	{  
+        printf("\nGRAMMAR :: %s",pro[i]);  
+        non=pro[i][0];  
+        if(non==pro[i][index])
+		{  
+            al=pro[i][index+1];  
+            printf(" is left recursive.\n");  
+            while(pro[i][index]!=0 && pro[i][index]!='|')  
+                index++;  
+            if(pro[i][index]!=0)
+			{  
+                be=pro[i][index+1];  
+                printf("Grammar without left recursion:\n");  
+                printf("%c->%c%c\'",non,be,non);  
+                printf("\n%c\'->%c%c\'|E\n",non,al,non);  
             }  
             else  
-                 printf(" is not left recursive.\n");  
-            index=3;  
-       }  
-  }   
+                printf(" can't be reduced\n");  
+        }  
+        else  
+            printf(" is not left recursive.\n");  
+        index=3;  
+    }  
+}
